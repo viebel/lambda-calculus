8d243fc13b05d4dffd49fce2bd6dd1cf9b255d8e
Yehonathan Sharvit <viebel@gmail.com>
2016-07-26 22:46:08 +0300

    Update macros.clj

--- src/lambda_calculus/macros.clj
+++ src/lambda_calculus/macros.clj
@@ -1,5 +1,5 @@
 (ns lambda-calculus.macros
-  (:require [lambda-calculus.core :refer [Lambda]]))
+  (:require [lambda-calculus.core :refer [->Lambda]]))
   
 (defmacro lambda [name args & body]
   `(defn ~name ~args (->Lambda ~@body)))
